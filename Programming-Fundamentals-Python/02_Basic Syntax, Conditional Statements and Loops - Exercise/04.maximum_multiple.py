575341d6125f8d6ac0eb368b68a42cec36a3728c
Vladislav Stoilov <oxus.home@gmail.com>
2024-09-13 19:08:46 +0300

    Added a file for Programming Fundamentals with Python.

--- Programming-Fundamentals-Python/02_Basic Syntax, Conditional Statements and Loops - Exercise/04.maximum_multiple.py
+++ Programming-Fundamentals-Python/02_Basic Syntax, Conditional Statements and Loops - Exercise/04.maximum_multiple.py
@@ -2,7 +2,7 @@ divisor = int(input())
 boundary = int(input())
 biggest_number = 0
 
-for i in range(1, boundary + 1):
+for i in range(boundary, 0, - 1):
     if i % divisor == 0:
         if i > biggest_number:
             biggest_number = i
