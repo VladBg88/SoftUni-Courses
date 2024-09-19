eb14068e99b87723074b437d491ed83a8b9f3a64
Vladislav Stoilov <oxus.home@gmail.com>
2024-09-19 22:22:52 +0300

    Added a code in Python Fundamentals.

---
+++ Programming-Fundamentals-Python/06_Data Types and Variables - More Exercises/01_exchange_integers.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = a
+d = b
+a = d
+b = c
+
+print('Before: \n'
+      f'a = {b}\n'
+      f'b = {a}\n'
+      f'After:\n'
+      f'a = {a}\n'
+      f'b = {b}')
