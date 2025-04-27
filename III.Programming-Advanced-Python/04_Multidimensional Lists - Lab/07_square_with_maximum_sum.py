965eb549f5057ac9f66eaf1d3cb3a6a78bde97a8
Vlad <oxus.home@gmail.com>
2025-04-27 11:54:04 +0300

    Adding solved exercises at SoftUni Advanced.

--- III.Programming-Advanced-Python/04_Multidimensional Lists - Lab/07_square_with_maximum_sum.py
+++ III.Programming-Advanced-Python/04_Multidimensional Lists - Lab/07_square_with_maximum_sum.py
@@ -1,31 +1,19 @@
 rows, cols = [int(x) for x in input().split(', ')]
-matrix = []
-biggest_submatrix = [0, 0, 0, 0, 0]
-
-for _ in range(rows):
-    matrix.append([int(x) for x in input().split(', ')])
+best_submatrix = [0, 0, 0, 0]
+best_sum = float('-inf')
+matrix = [[int(x) for x in input().split(', ')] for _ in range(rows)]
 
 for row in range(rows - 1):
     for col in range(cols - 1):
-        if sum([
-                matrix[row][col],
-                matrix[row][col + 1],
-                matrix[row + 1][col],
-                matrix[row + 1][col + 1]
-        ]) > biggest_submatrix[-1]:
-            biggest_submatrix = [
-                matrix[row][col],
-                matrix[row][col + 1],
-                matrix[row + 1][col],
-                matrix[row + 1][col + 1],
-                sum([
-                    matrix[row][col],
-                    matrix[row][col + 1],
-                    matrix[row + 1][col],
-                    matrix[row + 1][col + 1]
-                ])
+        current_submatrix = [
+                matrix[row][col : col + 2],
+                matrix[row + 1][col : col + 2],
             ]
+        current_sum = sum(sum(x) for x in current_submatrix)
+        if current_sum > best_sum:
+            best_submatrix, best_sum = current_submatrix, current_sum
 
-print(f"{biggest_submatrix[0]} {biggest_submatrix[1]}")
-print(f"{biggest_submatrix[2]} {biggest_submatrix[3]}")
-print(biggest_submatrix[-1])
+for nums in best_submatrix:
+    print(*nums)
+print(best_sum)
+
